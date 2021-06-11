4248843a6ff676e251d1b9198282f3adcca0768e
Park Yeung <yeungparkhay@gmail.com>
2021-06-11 16:10:04 +0100

    completed courseinfo

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,70 +1,67 @@
 import React from 'react'
+import Course from './components/Course'
 
 const App = () => {
-    const course = {
-        id: 1,
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10,
-                id: 1
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7,
-                id: 2
-            },
-            {
-                name: 'State of a component',
-                exercises: 14,
-                id: 3
-            }
-        ]
-    }
-    return <Course course={course} />
-}
-
-const Course = ({ course, parts }) => {
+    const courses = [
+        {
+            id: 1,
+            name: 'Half Stack application development',
+            parts: [
+                {
+                    name: 'Fundamentals of React',
+                    exercises: 10,
+                    id: 1
+                },
+                {
+                    name: 'Using props to pass data',
+                    exercises: 7,
+                    id: 2
+                },
+                {
+                    name: 'State of a component',
+                    exercises: 14,
+                    id: 3
+                },
+                {
+                    name: 'Redux',
+                    exercises: 11,
+                    id: 4
+                }
+            ]
+        },
+        {
+            id: 2,
+            name: 'Node.js',
+            parts: [
+                {
+                    name: 'Routing',
+                    exercises: 3,
+                    id: 1
+                },
+                {
+                    name: 'Middlewares',
+                    exercises: 7,
+                    id: 2
+                }
+            ]
+        }
+    ]
     return (
         <div>
-            <Header course={course.name}/>
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
+            <h1>Web development curriculum</h1>
+            <Courses courses={courses} />
         </div>
     )
 }
 
-const Header = ({ course }) => {
-    // Renders the name of the course
+const Courses = ({ courses }) => {
     return (
-        <h1>{course}</h1>
-    )
-}
-
-const Content = ({ parts }) => {
-    // Renders the three Part components
-    return (
-        parts.map((part) => (
+        courses.map((course) => (
             <div>
-                <Part part={part.name} exercises={part.exercises}/>
+                <Course course={course} />
             </div>
         ))
     )
 }
 
-const Part = ({ part, exercises }) => {
-    // Renders the parts of the course and the numbers of exercises of each
-    return (
-        <p>{part} {exercises}</p>
-    )
-}
-
-const Total = ({ parts }) => {
-    // Renders the total number of exercises
-    return (
-        <p>Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}</p>
-    )
-}
-
-export default App;
+export default App
