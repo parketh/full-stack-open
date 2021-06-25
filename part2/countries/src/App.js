8101ac12d8b24cdbd43367367576f05b3fb69428
yeungparkhay <yeungparkhay@gmail.com>
2021-06-25 13:13:29 +0100

    completed exercise 2.11

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -2,39 +2,58 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
 const App = () => {
-  const [ search, setSearch ] = useState('')
+  const [ searchPrompt, setSearchPrompt ] = useState('')
   const [ results, setResults ] = useState([])
-  const [ showProfile, setShowProfile ] = useState('display: block')
+  const [ filteredResults, setFilteredResults ] = useState([])
 
   useEffect(() => {
     axios
       .get('https://restcountries.eu/rest/v2/all')
       .then(response => {
-        setResults(response.data.map(country => country.name))
+        setResults(response.data.map(function(result) {
+          return {
+            "name": result.name,
+            "capital": result.capital,
+            "population": result.population,
+            "languages": result.languages.map(lang => lang.name),
+            "flag": result.flag,
+          }  
+        }))
       })
   }, [])
 
   const handlePromptChange = (event) => {
-    setSearch(event.target.value)
-    if (resultsToShow.length === 1) {
-      setShowProfile('display: none')
-    }
-    else {
-      setShowProfile('display: block')
-    }
+    setSearchPrompt(event.target.value)
+    setFilteredResults(results.filter(result => result.name.toLowerCase().search(event.target.value.toLowerCase()) !== -1))
   }
 
-  const filteredResults = results.filter(result => result.toLowerCase().search(search.toLowerCase()) !== -1)
+  console.log()
 
-  const resultsToShow = filteredResults.length > 10 ? ["Too many matches, specify another filter"] : filteredResults
-
-  return (
-    <div>
-      <Search label="find countries" prompt={search} handlePromptChange={handlePromptChange} />
-      <Results countries={resultsToShow}/>
-      <Profile countries={resultsToShow} showProfile={showProfile}/>
-    </div>
-  );
+  if (filteredResults.length > 10) {
+    return (
+      <div>
+        <Search label="find countries" prompt={searchPrompt} handlePromptChange={handlePromptChange} />
+        <span>Too many matches, specify another filter</span>
+      </div>
+    );
+  }
+  else if (filteredResults.length === 1) {
+    return (
+      <div>
+        <Search label="find countries" prompt={searchPrompt} handlePromptChange={handlePromptChange} />
+        <Profile countries={filteredResults}/>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div>
+        <Search label="find countries" prompt={searchPrompt} handlePromptChange={handlePromptChange} />
+        <Results countries={filteredResults}/>
+      </div>
+    );
+  }
+  
 }
 
 const Search = ({ label, prompt, handlePromptChange }) => {
@@ -50,16 +69,29 @@ const Results = ({ countries }) => {
   return (
     <div>
       {countries.map(country => (
-        <div>{country}</div>
+        <div>{country.name}</div>
       ))}
     </div>
   )
 }
 
-const Profile = ({ countries, showProfile }) => {
+const Profile = ({ countries }) => {
   return (
-    <div styles={showProfile}>
-        {countries.name}
+    <div>
+      {countries.map(country => (
+        <div>
+          <h2>{country.name}</h2>
+          <div>capital {country.capital}</div>
+          <div>population {country.population}</div>
+          <h3>languages</h3>
+          <ul>
+            {country.languages.map(language => (
+              <li>{language}</li>
+            ))}
+          </ul>
+          <img src={country.flag} width="100" />
+        </div>
+      ))}
     </div>
   )
 
