6048621c06b4ed941a3a08cfbc79de0b82e65da6
yeungparkhay <yeungparkhay@gmail.com>
2021-06-26 17:47:44 +0100

    completed exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,133 +1,142 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
-const api_key = process.env.REACT_APP_API_KEY
-
 const App = () => {
-  const [ searchPrompt, setSearchPrompt ] = useState('')
-  const [ results, setResults ] = useState([])
-  const [ filteredResults, setFilteredResults ] = useState([])
-  const [ profile, setProfile ] = useState('')
+    const [searchPrompt, setSearchPrompt] = useState('')
+    const [profile, setProfile] = useState([])
 
-  useEffect(() => {
-    axios
-      .get('https://restcountries.eu/rest/v2/all')
-      .then(response => {
-        setResults(response.data.map(function(result) {
-          return {
-            "name": result.name,
-            "capital": result.capital,
-            "population": result.population,
-            "languages": result.languages.map(lang => lang.name),
-            "flag": result.flag,
-          }  
-        }))
-      })
-  }, [])
+    const handlePromptChange = (event) => {
+        setSearchPrompt(event.target.value)
+        setProfile([])
+    }
 
-  const handlePromptChange = (event) => {
-    setSearchPrompt(event.target.value)
-    setFilteredResults(results.filter(result => result.name.toLowerCase().search(event.target.value.toLowerCase()) !== -1))
-    setProfile('')
-  }
-
-  const handleProfileChange = (event) => {
-    setProfile(event.target.id)
-  }
-
-  if (filteredResults.length > 10) {
     return (
-      <div>
-        <Search label="find countries" prompt={searchPrompt} handlePromptChange={handlePromptChange} />
-        <span>Too many matches, specify another filter</span>
-      </div>
-    );
-  }
-  else if (filteredResults.length === 1) {
-    return (
-      <div>
-        <Search label="find countries" prompt={searchPrompt} handlePromptChange={handlePromptChange} />
-        <Profile countries={filteredResults} profile={filteredResults[0].name}/>
-      </div>
-    );
-  }
-  else {
-    return (
-      <div>
-        <Search label="find countries" prompt={searchPrompt} handlePromptChange={handlePromptChange} />
-        <Results countries={filteredResults} handleProfileChange={handleProfileChange} profile={profile}/>
-      </div>
-    );
-  } 
-}
-
-const Search = ({ label, prompt, handlePromptChange }) => {
-  return (
-    <div>
-      <span>{label} </span>
-      <input value={prompt} onChange={handlePromptChange} />
-    </div>
-  )
-}
-
-const Results = ({ countries, handleProfileChange, profile }) => {
-  return (
-    <div>
-      {countries.map(country => (
         <div>
-          <span>{country.name} </span>
-          <input type="button" value="show" id={country.name} onClick={handleProfileChange}/>
+        <Search label="find countries" searchPrompt={searchPrompt} handlePromptChange={handlePromptChange} />
+        <Results searchPrompt={searchPrompt} profile={profile} setProfile={setProfile} />
         </div>
-      ))}
-      <Profile countries={countries} profile={profile}/>
-    </div>
-  )
+    )
+
 }
 
-const Profile = ({ countries, profile }) => {
-  const [ weather, setWeather ] = useState('')
-
-  const capital = encodeURI(countries.filter(country => country.name === profile).map(country => country.capital)[0])
-
-  useEffect(() => {
-    axios
-      .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${capital}`)
-      .then(response => {
-        setWeather(response.data)
-      })
-  }, [])
-
-  console.log("weather: " + JSON.stringify(weather))
-
-  return (
-    <div>
-      {countries.filter(country => country.name === profile).map(country => (
+const Search = ({ label, searchPrompt, handlePromptChange }) => {
+    return (
         <div>
-          <h2>{country.name}</h2>
-          <div>capital {country.capital}</div>
-          <div>population {country.population}</div>
-          <h3>languages</h3>
-          <ul>
-            {country.languages.map(language => (
-              <li>{language}</li>
-            ))}
-          </ul>
-          <img src={country.flag} alt={country.name} width="100" />
-          <h3>Weather in {country.name}</h3>
-          <div>
-            <b>temperature: </b>
-            <span>{weather.current.temperature} Celsius</span>
-          </div>
-          <img src={weather.current.weather_icons} alt={country.name} width="100" />
-          <div>
-            <b>wind: </b>
-            <span>{weather.current.wind_speed} mph direction {weather.current.wind_dir}</span>
-          </div>
+            <span>{label} </span>
+            <input value={searchPrompt} onChange={handlePromptChange} />
         </div>
-      ))}
-    </div>
-  )
+    )
+}
 
+const Results = ({ searchPrompt, profile, setProfile }) => {
+    const [countryData, setCountryData] = useState([])
+    
+    useEffect(() => {
+        axios
+            .get('https://restcountries.eu/rest/v2/all')
+            .then(response => {
+                setCountryData(response.data)
+            })
+    }, [])
+
+    let filteredCountries = countryData.filter(country => country.name.toLowerCase().search(searchPrompt.toLowerCase()) !== -1)
+    
+    const handleProfileChange = (event) => {
+        event.preventDefault()
+        setProfile(countryData.filter(country => country.name.search(event.target.id) !== -1)[0])
+    }
+
+    if (filteredCountries.length > 10) {
+        return (
+            <div>Too many matches, specify another filter</div>
+        )
+    }
+    else if (filteredCountries.length > 1) {
+        return (
+            <div>
+                {filteredCountries.map(country => (
+                    <div key={Number(country.numericCode)}>
+                        <span>{country.name} </span>
+                        <input type="button" value="show" id={country.name} onClick={handleProfileChange}/>
+                    </div>
+                ))}
+                <Profile profile={profile} />
+            </div>
+        )
+    }
+    else if (filteredCountries.length === 1) {
+        return (
+            <div>
+                <Profile profile={filteredCountries[0]} />
+            </div>
+        )
+    }
+    else {
+        return (
+            <div></div>
+        )
+    }
+
+}
+
+const Profile = ({ profile }) => {    
+    if (typeof profile.languages !== "undefined") {
+        return (
+            <div>
+                <h2>{profile.name}</h2>
+                <div>capital {profile.capital}</div>
+                <div>population {profile.population}</div>
+                <h3>languages</h3>
+                <ul>
+                    {profile.languages.map(l => (
+                        <li key={l.iso639_1}>{l.name}</li>
+                    ))}
+                </ul>
+                <Weather profile={profile} />
+            </div>
+        )
+    }
+    else {
+        return (
+            <div></div>
+        )
+    }
+}
+
+const Weather = ({ profile }) => {
+    const [weatherData, setWeatherData] = useState([])
+    
+    const api_key = process.env.REACT_APP_API_KEY
+
+    useEffect(() => {
+        axios
+          .get(`http://api.weatherstack.com/current?access_key=${api_key}&query=${encodeURI(profile.capital)}`)
+          .then(response => {
+            setWeatherData(response.data)
+          })
+      }, [])
+
+    if (weatherData.length !== 0) {
+        return (
+            <div>
+                <h3>Weather in {profile.capital}</h3>
+                <div>
+                    <b>temperature: </b>
+                    <span>{weatherData.current.temperature} Celsius</span>
+                </div>
+                <img src={weatherData.current.weather_icons} alt={profile.name} width="100" />
+                <div>
+                    <b>wind: </b>
+                    <span>{weatherData.current.wind_speed} mph direction {weatherData.current.wind_dir}</span>
+                </div>
+            </div>
+        )
+    }
+    else {
+        return (
+            <div></div>
+        )
+    }
 }
 
 export default App;
